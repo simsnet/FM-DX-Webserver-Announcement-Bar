8d9c29d3aa159fc84155524ce5904cc3bc91b041
Glenn <simstvnet@gmail.com>
2026-01-01 01:29:25 -0500

    Update to 2.0 with backend logic

--- AnnouncementBar.js
+++ AnnouncementBar.js
@@ -1,12 +1,13 @@
 // Plugin configuration, this is used in the administration when plugins are loaded
 var pluginConfig = {
     name: 'Announcement Bar',
-    version: '1.0',
+    version: '2.0',
     author: 'simsnet',
     frontEndPath: 'AnnouncementBar/AnnouncementBarCore.js'
 }
 
 // Backend (server) changes can go here...
+require('./AnnouncementBar/AnnouncementBar_server.js');
 
 // Don't change anything below here if you are making your own plugin
 module.exports = {
